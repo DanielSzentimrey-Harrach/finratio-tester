4fdb71bdd296bec55c0a28ccb2b9c0ad89d5d6d8
Daniel Szentimrey-Harrach <daniel.szhm@gmail.com>
2021-06-26 11:33:05 +0800

    Adding new ratios

--- src/app/finratios.ts
+++ src/app/finratios.ts
@@ -3,5 +3,11 @@ export const ratios: Map<string, string> = new Map<string, string>([
     ["Current Ratio", "Current Assets / Current Liabilities"],
     ["Quick Ratio", "(Current Assets - Inventories) / Current Liabilities"],
     ["Return on Equity (ROE)", "Net Income / Shareholders Equity"],
-    ["Net Profit Margin", "Net Profit / Net Sales"]
+    ["Net Profit Margin", "Net Profit / Net Sales"],
+    ["Return on Tangible Common Equity (ROTCE)0", "Net Income / (Shareholders Equity - Preferred Stock - Goodwill - Intangible Assets)"],
+    ["Working Capital Ratio", "Current Assets / Current Liabilities"],
+    ["Asset Turnover", "Revenues / Total Assets"],
+    ["Earning per Share (EPS)", "Net Income / Outstanding Shares"],
+    ["Price-Earnings (P/E) Ratio", "Current Stock Price / EPS"],
+    ["Equity Multiplier", "Total Assets / Common Equity"]
 ]);
